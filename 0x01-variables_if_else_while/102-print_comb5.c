37e64c5539f76fb92c760ac8659f76e3a5cc7fc3
iamGpower <powerex4greatness@gmail.com>
2022-03-12 06:41:32 +0100

    update: code, file naming adjustment and house keeping

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,27 +1,50 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
-* main - Entry point
-* putchar for printing numbers 00 to 99
-* Return: Always 0 (Success)
-*/
+ * main - main block
+ * Description: Print all possible combinations of two two-digit numbers.
+ * Numbers should range from 0 to 99.
+ * The two numbers should be separated by a space.
+ * All numbers should be printed with two digits. 1 should be printed as 01.
+ * Combination of numbers must be separated by a comma followed by a space.
+ * Combinations of numbers should be printed in ascending order.
+ * `00 01` and `01 00` are considered as the same combination.
+ * You can only use `putchar` to print to console.
+ * You can only use `putchar` up to 8 times.
+ * You are not allowed to use any variable of type `char`.
+ * Return: 0
+ */
 int main(void)
 {
-	int tens = '0';
-	int ones = '0';
+	int i, j;
+	int a, b, c, d;
 
-	for (tens = '0'; tens <= '9'; tens++)
+	for (i = 0; i < 100; i++)
 	{
-		for (ones = '0'; ones <= '9'; ones++)
+		a = i / 10; /* doubles fnum */
+		b = i % 10; /* singles fnum */
+
+		for (j = 0; j < 100; j++)
 		{
-			putchar(tens);
-			putchar(ones);
-			if (tens != '9' || ones != '9')
+			c = j / 10; /* doubles snum */
+			d = j % 10; /* singles snum */
+
+			if (a < c || (a == c && b < d))
 			{
-			putchar(',');
-			putchar(' ');
+				putchar(a + '0');
+				putchar(b + '0');
+				putchar(32);
+				putchar(c + '0');
+				putchar(d + '0');
+
+				if (!(a == 9 && b == 8))
+				{
+					putchar(44);
+					putchar(32);
+				}
 			}
 		}
 	}
 	putchar(10);
+
 	return (0);
 }
